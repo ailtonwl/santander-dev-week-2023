423051154b5c6f5c4cdbe13241c64cb62d563278
Ailton Martins Guimarães <ailton@workline.com.br>
2025-03-27 11:53:23 -0300

    Tratamento de Exceções

--- src/main/java/me/dio/controller/UserController.java
+++ src/main/java/me/dio/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
-    @PostMapping("/{id}")
+    @PostMapping
     public ResponseEntity<User> create(@RequestBody User userToCreate) {
         var userCreated = userService.create(userToCreate);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
